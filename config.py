91a6b968ef1d1ea965a6e5fe6293c877ff3aab42
Nazar <cutlass90@mail.ru>
2017-11-12 19:04:29 +0200

    refactoring implement test reader

--- config.py
+++ config.py
@@ -4,28 +4,29 @@ from dslib.tf_utils.config import (Config, IntegerParameter, RealParameter,
                                    StringParameter, CallableParameter, 
                                    Path, Scope, BooleanParameter,
                                    DictParameter, SequenceParameter)
+# in in word vectors PAD id is 0, and vector is np.zeros
+# in in word vectors UNK id is 1, and vector is np.ones
+
 config = Config(
     scope=Scope('answer_finder_my_prepro'),
     experiments_dir=Path('./experiments'),
     path = Path(os.path.abspath(__file__)),
 
-    path_to_vector_embeddings = Path('../datasets/glove/glove.840B.300d.txt'),
-    path_to_train_data = Path('./SQuAD/train.pkl'),
-    path_to_val_data = Path('./SQuAD/val.pkl'),
-    path_to_meta = Path('./SQuAD1/meta.msgpack'),
+    path_to_embeddings = Path('../datasets/glove/glove.840B.300d.txt'),
     path_to_train_json = Path('../datasets/SQuAD/train-v1.1.json'),
     path_to_val_json = Path('../datasets/SQuAD/dev-v1.1.json'),
+    path_to_train_data = Path('./SQuAD/train.pkl'),
+    path_to_val_data = Path('./SQuAD/val.pkl'),
+    path_to_meta = Path('./SQuAD/meta.pkl'),
     
-    question_size = IntegerParameter(40),
-    context_size = IntegerParameter(400),
+    question_size = IntegerParameter(30),
+    context_size = IntegerParameter(200),
     batch_size = IntegerParameter(64),
 
     hidden_size = IntegerParameter(256),
     n_rnn_layers = IntegerParameter(3),
     emb_size = IntegerParameter(300),
-    pos_size = IntegerParameter(50), #part-of-speech (POS)
-    ner_size = IntegerParameter(19), #named entity recognition (NER)
-    dict_size = IntegerParameter(91187),
+    dict_size = IntegerParameter(1000000),
     activation=StringParameter('tanh'),
 
     learn_rate = RealParameter(0.001),
@@ -34,6 +35,20 @@ config = Config(
     save_interval = IntegerParameter(1000),
     log_interval = IntegerParameter(1),
 
-    path_to_context = Path('./context.txt')
+    path_to_context = Path('./context.txt'),
+
+    vocab_tag = SequenceParameter(
+        ['<PAD>', '""', '#', '$', ',', '-LRB-', '-PRB-', '-RRB-', '.', ':', 'ADD',
+    'AFX', 'BES', 'CC', 'CD', 'DT', 'EX', 'FW', 'GW', 'HVS', 'HYPH', 'IN', 'JJ',
+    'JJR', 'JJS', 'LS', 'MD', 'NFP', 'NIL', 'NN', 'NNP', 'NNPS', 'NNS', 'PDT',
+    'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SP', 'SYM', 'TO', 'UH',
+    'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB', 'XX', '``'],
+    descriptive=False),
+
+    vocab_ent = SequenceParameter(
+        ['<PAD>',  '', 'ORG', 'DATE', 'PERSON', 'GPE', 'CARDINAL', 'NORP', 'LOC',
+        'WORK_OF_ART', 'PERCENT', 'EVENT', 'ORDINAL', 'MONEY', 'FAC', 'QUANTITY',
+        'LAW', 'TIME', 'LANGUAGE', 'PRODUCT'],
+        descriptive=False)
 
 )
--- tools_refact.py
+++ tools_refact.py
@@ -5,7 +5,10 @@ import os
 import collections
 import pickle
 import unicodedata
+from collections import defaultdict
 
+import csv
+import pandas as pd
 from tqdm import tqdm
 import spacy
 import numpy as np
@@ -164,6 +167,33 @@ def process_question(question, nlp):
     question_tokens = [normalize_text(w.text) for w in q_doc]
     return question_tokens
 
+def tokens_to_id(tokens, vocab, pad_symbol):
+    vocab = defaultdict(lambda: pad_symbol, vocab)
+    return np.array([vocab[t] for t in tokens])
+
+def get_meta(c):
+    glove_vectors = pd.read_table(c.path_to_embeddings, sep=" ", index_col=0,
+        header=None, quoting=csv.QUOTE_NONE)
+    indexes = []
+    with open(c.path_to_embeddings) as f:
+        for line in f:
+            indexes.append(normalize_text(line.rstrip().split(' ')[0]))
+    glove_vectors = pd.DataFrame(glove_vectors.as_matrix(), index=indexes)
+    glove_vectors = glove_vectors[:c.dict_size-2]
+    pad = pd.DataFrame(np.zeros([1, c.emb_size]), index=['<PAD>'])
+    unk = pd.DataFrame(np.ones([1, c.emb_size]), index=['<UNK>'])
+    glove_vectors = pd.concat([pad, unk, glove_vectors], axis=0)
+    embeddings = glove_vectors.as_matrix()
+    w2id = {w:i for i, w in enumerate(glove_vectors.index)}
+    tag2id = {w:i for i, w in enumerate(c.vocab_tag)}
+    ent2id = {w:i for i, w in enumerate(c.vocab_ent)}
+
+    meta = {'emb':embeddings, 'w2id':w2id, 'tag2id':tag2id, 'ent2id':ent2id}
+    os.makedirs(os.path.dirname(c.path_to_meta), exist_ok=True)    
+    with open(c.path_to_meta, 'wb') as f:
+        pickle.dump(meta, f)
+
+    return embeddings, w2id, tag2id, ent2id
 ######################## gather functions ######################################
 
 def read_set(path_to_json, path_to_save):
@@ -180,7 +210,7 @@ def read_set(path_to_json, path_to_save):
     paragraphs = []
     data = json.load(open(path_to_json, 'r'))
     data = [parag for title_data in data['data'] for parag in title_data['paragraphs']]
-    for parag in tqdm(data[:10]):
+    for parag in tqdm(data):
         qas = []
         for qa in parag['qas']:
             answers = []
@@ -192,5 +222,78 @@ def read_set(path_to_json, path_to_save):
         pickle.dump(paragraphs, f)
     return paragraphs
 
+def load_pickled_object(path):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+def read(paragraph, w2id, tag2id, ent2id, c):
+    """ Conver to id, make padding and prepare data for training. """
+    def filler(data, container, max_size, s=None):
+        size = len(data)
+        if size <= max_size:
+            container[:size] = data
+            s = None
+        else:
+            if s is None:
+                s = random.randint(0, size - max_size)
+            container[:] = data[s:s + max_size]
+        return container, s
+    # import pdb
+    # pdb.set_trace()
+    #get data
+    qa = paragraph.get_qa()
+    answer = qa.get_answer()
+    a_s, a_e = answer.start_token, answer.end_token
+    
+    # convert to id
+    question_id = tokens_to_id(qa.question_tokens, w2id, 1)
+    context_id = tokens_to_id(paragraph.context_tokens, w2id, 1)
+    pos_id = tokens_to_id(paragraph.context_tags, tag2id, 0)
+    ner_id = tokens_to_id(paragraph.context_ents, ent2id, 0)
+    
+    # create blank
+    question = np.zeros([c.question_size], dtype=int)
+    context = np.zeros([c.context_size], dtype=int)
+    pos = np.zeros([c.context_size], dtype=int)
+    ner = np.zeros([c.context_size], dtype=int)
+    context_features = -1*np.ones([c.context_size, 4], dtype=float)
+
+    # fill blank
+    # pos_size = len(c.vocab_tag)
+    # ner_size = len(c.vocab_ent)
+    question, _ = filler(question_id, question, c.question_size)
+    context, s = filler(context_id, context, c.context_size)
+    pos, _ = filler(pos_id, pos, c.context_size, s)
+    ner, _ = filler(ner_id, ner, c.context_size, s)
+    context_features, _ = filler(qa.context_featurs, context_features,
+                                 c.context_size, s)
+    print('s', s)
+    if s is None:
+        answer = np.zeros([c.context_size], dtype=float)
+        answer[a_s:a_e] = 1
+    else:
+        answer = np.zeros([len(context_id)])
+        answer[a_s:a_e] = 1
+        answer = answer[s:s+c.context_size]
+    
+    return [(question, context, pos, ner, context_features, answer)]
+
+
+
+class Reader:
+
+    def __init__(self, w2id, tag2id, ent2id, c):
+        [setattr(self, name, val) for name,val in locals().items() if name != 'self']
+
+
+
 if __name__ == '__main__':
-    paragraphs = read_set('../../../datasets/SQuAD/dev-v1.1.json', './SQuAD/temp.pkl')
+    from config import config as c
+    # read_set(c.path_to_train_json, c.path_to_train_data)
+    # read_set(c.path_to_test_json, c.path_to_est_data)
+    # get_meta(c)
+
+    w2id, tag2id, ent2id = load_pickled_object('./w_tag_ent.pkl')
+    data = load_pickled_object('./SQuAD/train.pkl')
+    read(data[0], w2id, tag2id, ent2id, c)
