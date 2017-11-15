b6a67321a4b39b3bf4b46d93eb94607e4448d405
Nazar <cutlass90@mail.ru>
2017-11-15 13:23:00 +0200

    chages

--- inference_on_list_of_questions.py
+++ inference_on_list_of_questions.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-
+import os
+import time
 import matplotlib
 matplotlib.use('Agg')
 from wikiapi import WikiApi
@@ -9,6 +10,10 @@ import tools
 from answer_finder import AnswerFinder
 from config import config as c
 
+
+with open('./questions.txt') as f:
+    questions = [line[:-1] for line in f]
+
 model = AnswerFinder(config=c,
                     restore=True,
                     mode="inference")
@@ -17,6 +22,7 @@ print('''Hello! This is Alpha version of program for reading wikipedia to answer
 Program was writing basing on paper https://arxiv.org/pdf/1704.00051.pdf
 For more detail cutlass900@gmail.com\n''')
 
+c.inf_threshold = 0.7
 while True:
     while True:
         print('What or who do you want to ask about? Example: Barak Obama')
@@ -27,15 +33,17 @@ while True:
             break
         else:
             print('Can\'t find any wiki pages about {}. Try another one.'.format(thing))
+    
     article = wiki.get_article(results[0])
     context = article.content
-    while True:
-        print('What is your question?')
-        question = input()
-        print('Your question is {}'.format(question))
+    for question in questions:
+        os.system('clear')
+        print('Q: {}'.format(question))
         print('Search answers ...')
         answers, probs = tools.get_answer(context, question, model, c)
         print('Found {} answer(s):'.format(len(answers)))
         for i, (a, p) in enumerate(zip(answers, probs)):
             print(str(i+1), a, ', probability = {0:.2f}'.format(p))
         print()
+        print()
+        time.sleep(5)
